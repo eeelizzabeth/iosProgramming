9c69fd42af0669bfc7a67a53304574b60c3f5d10
Elizabeth Hernandez <eeelizabeth@elizabeths-air.attlocal.net>
2019-10-15 10:30:10 -0700

    Twitter part 2

--- Twitter/Twitter/HomeTableViewController.swift
+++ Twitter/Twitter/HomeTableViewController.swift
@@ -15,17 +15,54 @@ class HomeTableViewController: UITableViewController {
     //number of tweets you want
     var numberOfTweets: Int!
 
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
 
     }
     
-    func loadTweet() {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
+    }
+    
+    @objc func loadTweets() {
+        numberOfTweets = 20
+        
         //request tweets url
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         //params are set to a count of 10 tweets
-        let myParams = ["count": 10]
+        let myParams = ["count": numberOfTweets]
+        
+        //grab the tweets through the API call
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            //clean up the tweets before appending them to tweetArray
+            self.tweetArray.removeAll()
+            
+            //loop through the tweets in API call
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+            
+        }, failure: { (Error) in
+            print("could not retrieve tweets! oh no!")
+        })
+
+    }
+    
+    func loadMoreTweets() {
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": numberOfTweets]
+        numberOfTweets = numberOfTweets + 20
         
         //grab the tweets through the API call
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
@@ -42,10 +79,14 @@ class HomeTableViewController: UITableViewController {
         }, failure: { (Error) in
             print("could not retrieve tweets! oh no!")
         })
-        
-        
     }
     
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath)
+    {
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
     
     
     @IBAction func onLogout(_ sender: Any) {
@@ -71,6 +112,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorited(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell;
     }
